684e6bd5fa5bd2b9cfe5fd69199a75067ef9be5d
Sonali9910 <sonalibaddal2110@gmail.com>
2023-03-31 01:00:31 +0530

    Displayed data Added Validation

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -9,15 +9,28 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        private ApplicationDbContext _context;
+        public CreateTaskModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty]
         public Task NewTask { get; set; }
+
         public void OnGet()
         {
+
         }
 
         public IActionResult OnPost()
         {
+            _context.Tasks.Add(NewTask);
+            _context.SaveChanges();
             return RedirectToPage("Index");
         }
+
+        
     }
+
 }
